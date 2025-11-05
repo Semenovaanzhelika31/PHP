4a7308d4c0b3df65cac0c2d9906e29a0612da0b3
Semenovaanzhelika31 <semnovalika@gmail.com>
2025-11-05 14:14:12 +0300

    Create lib.inc.php

---
+++ incc/inc/lib.inc.php
@@ -0,0 +1,29 @@
+<?php
+function getTable($cols = 5, $rows = 5, $color = '#ff0000') {
+    echo '<table border="1" width="200" style="border-collapse: collapse;">';
+    for ($i = 1; $i <= $rows; $i++) {
+        echo '<tr>';
+        for ($j = 1; $j <= $cols; $j++) {
+            $product = $i * $j;
+            echo '<td style="background-color: ' . htmlspecialchars($color) . '; color: white; text-align: center; padding: 5px;">' . $product . '</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+}
+
+function getMenu() {
+    $menuItems = [
+        'index.php' => 'Домой',
+        'about.php' => 'О нас',
+        'contact.php' => 'Контакты',
+        'table.php' => 'Таблица умножения',
+        'calc.php' => 'Калькулятор'
+    ];
+    echo '<ul>';
+    foreach ($menuItems as $url => $text) {
+        echo '<li><a href="' . htmlspecialchars($url) . '">' . htmlspecialchars($text) . '</a></li>';
+    }
+    echo '</ul>';
+}
+?>
