683cbb0c49aa7ffb1d3beb065e98229d941e977c
Semenovaanzhelika31 <semnovalika@gmail.com>
2025-11-05 14:42:16 +0300

    Create data.inc.php

---
+++ lab4/inc/data.inc.php
@@ -0,0 +1,15 @@
+<?php
+// Параметры для таблицы умножения
+$defaultCols = 5;
+$defaultRows = 5;
+$defaultColor = '#ff0000';
+
+// Меню навигации (все ссылки ведут на index.php с параметром id)
+$menuItems = [
+    'index.php'           => 'Домой',
+    'index.php?id=about'  => 'О нас',
+    'index.php?id=contact' => 'Контакты',
+    'index.php?id=table'  => 'Таблица умножения',
+    'index.php?id=calc'   => 'Калькулятор'
+];
+?>
