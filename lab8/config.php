3254937d9b2fe1abffe072c19623419ca497aaaa
Semenovaanzhelika31 <semnovalika@gmail.com>
2025-12-02 10:11:44 +0300

    Create config.php

---
+++ lab8/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME', 'f1176552_login_db');
+define('DB_USER', 'f1176552_login_db');
+define('DB_PASSWORD', 'Rmxskromy9!');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+?>
