b8e65a07aa4063b242ea03a0f7909d5c9dfd7292
Semenovaanzhelika31 <semnovalika@gmail.com>
2025-12-04 09:37:17 +0300

    Update top.inc.php

--- incc/inc/top.inc.php
+++ incc/inc/top.inc.php
@@ -1,2 +1,5 @@
-<img src="logo.png" width="130" height="80" alt="Наш логотип" class="logo">
+
+<a href="index.php" class="logo-link">
+  <img src="logo.png" width="130" height="80" alt="Наш логотип" class="logo">
+</a>
 <span class="slogan">приходите к нам учиться</span>
