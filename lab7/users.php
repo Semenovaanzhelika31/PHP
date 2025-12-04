cab5aa65254246717c6ae945c4897a97ca1b45ca
Semenovaanzhelika31 <semnovalika@gmail.com>
2025-12-04 11:06:49 +0300

    Update users.php

--- lab7/users.php
+++ lab7/users.php
@@ -1,30 +1,64 @@
 <?php
 
-spl_autoload_register(function ($class) {
-    $classPath = str_replace('\\', '/', $class);
-    $file = __DIR__ . '/' . $classPath . '.php';
-    if (file_exists($file)) {
-        require_once $file;
-    } else {
-        throw new Exception("Файл класса не найден: $file");
+spl_autoload_register(
+    function ($class) {
+        $classPath = str_replace(
+            '\\',
+            '/',
+            $class
+        );
+        
+        $file = __DIR__ . '/' . $classPath . '.php';
+        
+        if (file_exists($file)) {
+            require_once $file;
+        } else {
+            throw new Exception(
+                "Файл класса не найден: $file"
+            );
+        }
     }
-});
+);
 
 use MyProject\Classes\User;
 use MyProject\Classes\SuperUser;
 
-$user1 = new User("Иван Иванов", "ivan", "12345");
-$user2 = new User("Анна Петрова", "anna", "54321");
-$superUser = new SuperUser("Администратор", "admin", "adminpass", "Главный");
+$user1 = new User(
+    "Иван Иванов",
+    "ivan",
+    "12345"
+);
 
-echo "<!DOCTYPE html><html lang='ru'><head><meta charset='UTF-8'></head><body>";
-echo "<h1>Информация о пользователях</h1>";
+$user2 = new User(
+    "Анна Петрова",
+    "anna",
+    "54321"
+);
 
-$user1->showInfo();
-$user2->showInfo();
+$superUser = new SuperUser(
+    "Администратор",
+    "admin",
+    "adminpass",
+    "Главный"
+);
 
-echo "<h2>Суперпользователь</h2>";
-$superUser->showInfo();
-
-echo "</body></html>";
 ?>
+
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Информация о пользователях</title>
+    </head>
+    <body>
+        <h1>Информация о пользователях</h1>
+        
+        <?php $user1->showInfo(); ?>
+        
+        <?php $user2->showInfo(); ?>
+        
+        <h2>Суперпользователь</h2>
+        
+        <?php $superUser->showInfo(); ?>
+    </body>
+</html>
