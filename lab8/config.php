215b81f358a307e6cb90aad11ac2c4966d539634
Semenovaanzhelika31 <semnovalika@gmail.com>
2025-12-04 10:16:44 +0300

    Delete lab8/config.php

--- lab8/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_NAME', 'f1176552_login_db');
-define('DB_USER', 'f1176552_login_db');
-define('DB_PASSWORD', 'Rmxskromy9!');
-define('DB_HOST', 'localhost');
-define('DB_CHARSET', 'utf8');
-?>
